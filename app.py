c4f026f975b30c0034e35d8fe733b7f2dfa50ce5
Andrei Popescu <andpopes@gmail.com>
2016-03-11 15:28:22 -0800

    New submission

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, redirect
+import datetime as dt
+from urllib2 import urlopen
+from json import load as Jload
+import pandas as pd
+from bokeh.plotting import figure, output_file, save
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+  return redirect('/index')
+
+@app.route('/index')
+def index():
+  return render_template('index.html')
+
+@app.route('/get_data', methods=['POST'])
+def get_data():
+    data = ExtractData(request.form['ticker'])
+    graph_file = BuildGraph(data, request.form['ticker'])
+    if graph_file:
+        return render_template(graph_file)
+    return 'ERROR: Data cannot be extracted. Check the ticker or try again later'
+
+
+def BuildGraph(data, tick):
+    if data.empty:
+        return []
+    output_file("./templates/graph.html")
+    p = figure(
+            tools="pan,box_zoom,reset,save", title='Closing price for last month',
+            x_axis_label='Days', y_axis_label='Closing Price'
+            )
+    try:
+        p.line(range(data.shape[0]), data['Close'], legend=tick)
+        save(p)
+        name = 'graph.html'
+    except:
+        name = ''
+    return name
+
+
+def ExtractData(ticker):
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    today = dt.date.today()
+    nday = today.day
+    nyear = today.year
+    nmonth = today.month - 1
+    if not nmonth:
+        nmonth = 12
+        nyear = nyear - 1
+    if nday > days[nmonth]:
+        nday = days[nmonth]
+    start = '%d-%d-%d'%(nyear, nmonth, nday)
+    stop = '%d-%d-%d'%(today.year, today.month, today.day)
+    url = 'https://www.quandl.com/api/v3/datasets/WIKI/%s.json?start_date=%s&end_date=%s'%(ticker, start, stop)
+    try:
+        data = Jload(urlopen(url))['dataset']
+        data = pd.DataFrame(data['data'], columns=data['column_names'])
+    except:
+        data = []
+    return data
+    
+
+
+if __name__ == '__main__':
+  app.run()
